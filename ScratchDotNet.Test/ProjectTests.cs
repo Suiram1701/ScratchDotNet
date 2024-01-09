45e503dc31d155c6c9780a95ece4be631118c8ba
Marius Kaufman <marius.kaufman@web.de>
2024-01-09 22:08:47 +0100

    Add a test for reading the project metadata

---
+++ ScratchDotNet.Test/ProjectTests.cs
@@ -0,0 +1,25 @@
+﻿using ScratchDotNet.Core;
+
+namespace ScratchDotNet.Test;
+
+/// <summary>
+/// Some tests for the projects
+/// </summary>
+[TestFixture]
+internal class ProjectTests
+{
+    /// <summary>
+    /// Tests reading of the project metadata
+    /// </summary>
+    [Test]
+    public async Task ReadMetadataAsync()
+    {
+        Metadata metadata = await Setup.Project.GetMetadataAsync();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(metadata.Vm, Is.Not.Empty, "Unexpected {0} string at Property {1}", nameof(string.Empty), nameof(Metadata.Vm));
+            Assert.That(metadata.UserAgent, Is.Not.Empty, "Unexpected {0} string at Property {1}", nameof(string.Empty), nameof(Metadata.UserAgent));
+        });
+    }
+}
--- ScratchDotNet.Test/Setup.cs
+++ ScratchDotNet.Test/Setup.cs
@@ -9,11 +9,30 @@ namespace ScratchDotNet.Test;
 [SetUpFixture]
 internal class Setup
 {
+    public static Project Project { get; private set; }
+
     public static JObject BlocksToken { get; private set; }
 
     public static ILoggerFactory LoggerFactory { get; private set; }
 
+    private const string _projectPath = "Sample/Scratch-Projekt.sb3";
     private const string _fileName = "Sample/project.json";
+
+    [OneTimeSetUp]
+    public void SetupLogging()
+    {
+        LoggerFactory = new LoggerFactory(new[]
+        {
+            new NUnitLoggerProvider()
+        });
+    }
+
+    [OneTimeSetUp]
+    public void ProjectSetup()
+    {
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _projectPath);
+        Project = new Project(filePath, LoggerFactory);
+    }
 
     [OneTimeSetUp]
     public async Task SourceSetupAsync()
@@ -26,14 +45,5 @@ internal class Setup
         using JsonReader blocksReader = rootObj.SelectToken("targets[1].blocks")!.CreateReader();
 
         BlocksToken = await JObject.LoadAsync(blocksReader);
-    }
-
-    [OneTimeSetUp]
-    public void SetupLogging()
-    {
-        LoggerFactory = new LoggerFactory(new[]
-        {
-            new NUnitLoggerProvider()
-        });
     }
 }
