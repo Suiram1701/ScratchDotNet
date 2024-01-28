c7c72a54cdf300d24c8dc41f045c194dd633aad2
Marius Kaufman <marius.kaufman@web.de>
2024-01-28 20:53:00 +0100

    Add the parse tests for the contol blocks

---
+++ ScratchDotNet.Test/Parse/ControlTests.cs
@@ -0,0 +1,64 @@
+﻿using ScratchDotNet.Test.StartCodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchDotNet.Test.Parse;
+
+/// <summary>
+/// Tests for control blocks
+/// </summary>
+[TestFixture]
+internal class ControlTests
+{
+    /// <summary>
+    /// Test for the blocks 'wait' and 'wait until'
+    /// </summary>
+    [Test]
+    public void Wait() =>
+        TestHelpers.DoParseTest(ControlCodes.WaitCode);
+
+    /// <summary>
+    /// Test fthe blocks 'repeat', 'repeat until' and 'repeat forever'
+    /// </summary>
+    [Test]
+    public void Repeat() =>
+        TestHelpers.DoParseTest(ControlCodes.ReapeatCode);
+
+    /// <summary>
+    /// Test for the blocks 'if' and 'If else'
+    /// </summary>
+    [Test]
+    public void IfElse() =>
+        TestHelpers.DoParseTest(ControlCodes.IfElseCode);
+
+    /// <summary>
+    /// Test for the block 'stop this script'
+    /// </summary>
+    [Test]
+    public void StopThisScript() =>
+        TestHelpers.DoParseTest(ControlCodes.StopThisSciptCode);
+
+    /// <summary>
+    /// Test for the block 'stop all scripts'
+    /// </summary>
+    [Test]
+    public void StopAll() =>
+        TestHelpers.DoParseTest(ControlCodes.StopAllCode);
+
+    /// <summary>
+    /// Test for the block 'stop other script in sprite'
+    /// </summary>
+    [Test]
+    public void StopOtherScripts() =>
+        TestHelpers.DoParseTest(ControlCodes.StopOtherScriptsCode);
+
+    /// <summary>
+    /// Test for the blocks 'When I start as a clone', 'create clone of myself', 'create clone of' and 'delete this clone'
+    /// </summary>
+    [Test]
+    public void Clone() =>
+        TestHelpers.DoParseTest(ControlCodes.CloneCode);
+}
---
+++ ScratchDotNet.Test/StartCodes/ControlCodes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchDotNet.Test.StartCodes;
+
+internal static class ControlCodes
+{
+    public const string WaitCode = "?1iO|n?MW?ux{guLuI(m";
+
+    public const string ReapeatCode = "Gco|v]ZkQ,VI*PRXj3_f";
+
+    public const string IfElseCode = "]5*^tlCM{Rx485/kjnTf";
+
+    public const string StopThisSciptCode = "9MJ#oF%!blk!MxQ*d#:+";
+
+    public const string StopAllCode = "U^{W=({c9N%):]ac*;jF";
+
+    public const string StopOtherScriptsCode = ")NBcc2QmH4`q-q(waG3T";
+
+    public const string CloneCode = "s5(:KedB2@.dF|o~BGR";
+}
